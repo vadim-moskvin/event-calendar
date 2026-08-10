d8f49b213d281ff0002bf38f3e4eac7da2d3d8e1
Vadim Moskvin <vadim.moskvin@yandex.by>
2026-08-10 14:57:57 +0300

    Configure DbContext

---
+++ src/EventCalendar/DataAccess/AppDbContext.cs
@@ -0,0 +1,15 @@
+using EventCalendar.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventCalendar.DataAccess;
+
+internal sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Event> Events => Set<Event>();
+    public DbSet<Booking> Bookings => Set<Booking>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
