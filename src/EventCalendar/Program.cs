a3dfbc0a45510089eb5f3d3a1be11c301a3337b6
Vadim Moskvin <vadim.moskvin@yandex.by>
2026-09-10 18:22:10 +0300

    Create initial migration

--- src/EventCalendar/Program.cs
+++ src/EventCalendar/Program.cs
@@ -34,7 +34,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
