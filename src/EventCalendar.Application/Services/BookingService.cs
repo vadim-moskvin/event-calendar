7ced67ac10090a84130ce2cf32417ca6ccf8140c
Vadim Moskvin <vadim.moskvin@yandex.by>
2026-09-14 17:16:13 +0300

    Change locking in BookingService to per event

--- src/EventCalendar.Application/Services/BookingService.cs
+++ src/EventCalendar.Application/Services/BookingService.cs
@@ -1,4 +1,5 @@
-﻿using EventCalendar.Application.Repositories;
+﻿using System.Collections.Concurrent;
+using EventCalendar.Application.Repositories;
 using EventCalendar.Domain.Exceptions;
 using EventCalendar.Domain.Models;
 
@@ -8,11 +9,13 @@ public class BookingService(IEventService eventService, IBookingRepository booki
 {
     private const string BookingNotFoundException = "Бронь не найдена";
 
-    private static readonly SemaphoreSlim SemaphoreSlim = new(1, 1);
+    private static readonly ConcurrentDictionary<Guid, SemaphoreSlim> EventLocks = new();
 
     public async Task<Booking> CreateBookingAsync(Guid eventId)
     {
-        await SemaphoreSlim.WaitAsync();
+        var semaphore = EventLocks.GetOrAdd(eventId, _ => new SemaphoreSlim(1, 1));
+
+        await semaphore.WaitAsync();
 
         try
         {
@@ -27,7 +30,7 @@ public class BookingService(IEventService eventService, IBookingRepository booki
         }
         finally
         {
-            SemaphoreSlim.Release();
+            semaphore.Release();
         }
     }
 
